f7ad9ca60aa4c5d88ac2bfee71df433913c45e44
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2024-07-25 15:38:57 -0400

    updated localization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,25 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, SignUp } from "@clerk/nextjs";
 import { TopNav } from "./_components/topnav";
-
+import { zhTW } from "@clerk/localizations";
+import { frFR } from "@clerk/localizations";
 export const metadata = {
   title: "OCM Visitors",
   description: "Created by Benjamin Cheung",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
+
+
+const localization = {
+  signUp: {
+    start: {
+      title: "登记",
+
+    }
+  }
+}
 
 export default function RootLayout({
   children,
@@ -16,8 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-
-    <ClerkProvider>
+    <ClerkProvider localization={localization}>
     <html lang="en" className={`${GeistSans.variable}`}>
       <body className="font-sans">
         <TopNav />
